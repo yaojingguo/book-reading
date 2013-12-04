3aabd2589916cf1a08063f0367476d9704c5aaa7
Jingguo Yao <yaojingguo@gmail.com>
2013-12-04 15:23:41 +0800

    Up

--- programming_pearls/column1/lazy_bit_vector.cc
+++ programming_pearls/column1/lazy_bit_vector.cc
@@ -5,28 +5,53 @@
 #include <assert.h>
 #include <string.h>
 
-#define N 10000000
+#define N 2000
 
 #define BITSPERWORD 32
 #define SHIFT 5
 #define MASK 0x1F
+#define SIZE (1 + N/BITSPERWORD)
 
-int b[1 + N/BITSPERWORD];
+int b[SIZE];
+int from[SIZE];
+int to[SIZE];
+int top = 0;
+
+void setup(int i)
+{
+  if (from[i] < top && to[from[i]] == i) 
+    return;
+  b[i] = 0;
+  from[i] = top;
+  to[top] = i;
+  top++;
+}
 
 void set(int i) {
-  b[i>>SHIFT] |= (1 << (i & MASK));
+  int idx = i>>SHIFT;
+  setup(idx);
+  b[idx] |= (1 << (i & MASK));
 }
 
 void clr(int i) {
-  b[i>>SHIFT] &= ~(1 << (i & MASK));
+  int idx = i>>SHIFT;
+  setup(idx);
+  b[idx] &= ~(1 << (i & MASK));
 }
 
 int test(int i) {
-  return b[i>>SHIFT] & (1 << (i & MASK));
+  int idx = i>>SHIFT;
+  setup(idx);
+  return b[idx] & (1 << (i & MASK));
 }
 
 int test_bit_vector()
 {
+  // Since b is in data section, its elements will be initialized to 0. The 
+  // following code to scramble the contents of array b.
+  for (int i = 0; i < SIZE; i++)
+    b[i] = 0xFFFFFFFF;
+
   set(10);
   assert(test(10) > 0);
   assert(test(9) == 0);
