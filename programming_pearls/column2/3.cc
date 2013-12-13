29b66dce06c3495de5d57966b60db32d7021e4c4
Jingguo Yao <yaojingguo@gmail.com>
2013-12-13 17:58:31 +0800

    Up

--- programming_pearls/column2/3.cc
+++ programming_pearls/column2/3.cc
@@ -1,5 +1,17 @@
+#include <string.h>
+#include <stdio.h>
+
+void left_rotate(char* x, int i)
+{
+  char t;
+  int n, j, k;
+
+  n = strlen(x);
+}
+
 int main(int argc, const char *argv[]) 
 {
-  
+  char x[] = "abcdefgh";
+  left_rotate(x, 3);
   return 0;
 }
