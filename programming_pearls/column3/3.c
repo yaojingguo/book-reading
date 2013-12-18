8d83eb7f5a9715f8f0e81de83583ea86478fa860
Jingguo Yao <yaojingguo@gmail.com>
2013-12-18 17:09:37 +0800

    Up

--- programming_pearls/column3/3.c
+++ programming_pearls/column3/3.c
@@ -1,16 +1,40 @@
 #include <stdio.h>
+#include <assert.h>
 
-char* banner = 
-"x        x\n"
-" x      x \n"
-"   x   x  \n"
-"     x    \n"
-"   x   x  \n"
-" x      x \n"
-"x        x\n";
+#define N 100
+char buf[N];
+
+void banner(char *s)
+{
+  char c;
+  int i, j, row, col, cnt, printed;
+
+  i = 0;
+  while (s[i] != '\0') {
+    row = s[i++] - '0';
+    cnt = 0;
+    while (s[i] != '\n' && s[i] != '\0') {
+      col = s[i++] - '0';
+      c = s[i++];
+      for (j = 0; j < col; j++) {
+        printed = snprintf(buf + cnt, N - cnt, "%c", c == 'b' ? ' ' : c);
+        assert(printed == 1);
+        cnt += printed;
+      }
+    }
+    for (j = 0; j < row; j++)
+      printf("%s\n", buf);
+    if (s[i] == '\n') {
+      i++;
+    }
+  }
+  printf("\n");
+}
 
 int main(int argc, const char *argv[]) 
 {
-  printf("%s\n", banner);
+  banner("39x\n"
+         "63b3x3b\n"
+         "39x");
   return 0;
 }
--- programming_pearls/column3/4.c
+++ programming_pearls/column3/4.c
@@ -1,8 +1,74 @@
 #include <stdio.h>
 
-char *month_names[2] = {"", ""};
+#define TRUE  1
+#define FALSE 0
+
+char *month_names[] = {"January", "February", "March", "April", "May", "June", 
+  "July", "August", "September", "October", "November", "December"};
+
+//                   1   2   3   4   5   6   7   8   9   10  11  12
+int months[][12] = {{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+                    {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
+
+int years[] = {365, 366};
+
+struct date {
+  int year;
+  int month;
+  int day;
+};
+
+int is_leap(int year) 
+{
+  if (year % 400 == 0) return TRUE;
+  if (year % 4 == 0 && year % 100 != 0) return TRUE;
+  return FALSE;
+}
+
+// int days_in_year()
+// {
+//   int cnt, i;
+//   for (i = 0, cnt = 0; i < 12; i++)
+//     cnt += months[i];
+// }
+
+int days(struct date a, struct date b)
+{
+  int cnt;
+
+  cnt = 0;
+
+}
+
+int weekday(struct date a)
+{
+  int days = months[is_leap(a.year)][a.month - 1];
+}
+
+void calendar(int year, int month)
+{
+  int i;
+  printf("       %d\n"
+         "      %s\n"
+         // "Su Mo Tu We Th Fr Sa", year, month_names[month - 1]);
+         "====================", year, month_names[month - 1]);
+  for (i = 0; i < months[is_leap(year)][month - 1]; i++) {
+    if (i % 7 == 0) printf("\n");
+    printf("%-3d", i + 1);
+  }
+  printf("\n");
+}
+
+void test_calendar()
+{
+  calendar(2010, 2);
+  calendar(2012, 2);
+  calendar(2013, 11);
+  calendar(2013, 12);
+}
 
 int main(int argc, const char *argv[]) 
 {
+  test_calendar();
   return 0;
 }
