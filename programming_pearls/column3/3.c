a350d309673419a88f429f6e2a7036e9cad9cf5d
Jingguo Yao <yaojingguo@gmail.com>
2013-12-18 14:31:14 +0800

    Up

---
+++ programming_pearls/column3/3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+char* banner = 
+"x        x\n"
+" x      x \n"
+"   x   x  \n"
+"     x    \n"
+"   x   x  \n"
+" x      x \n"
+"x        x\n";
+
+int main(int argc, const char *argv[]) 
+{
+  printf("%s\n", banner);
+  return 0;
+}
