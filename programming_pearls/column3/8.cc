f50077caeb155c030a372cb847099031601e9bff
Jingguo Yao <yaojingguo@gmail.com>
2013-12-19 14:08:56 +0800

    Up

--- programming_pearls/column3/8.cc
+++ programming_pearls/column3/8.cc
@@ -3,18 +3,27 @@
 unsigned char table[] = {0x7D, 0x50, 0x37, 0x57, 0x5A,
                          0x4F, 0x6F, 0x54, 0x7F, 0x5E};
 
-void display(unsigned short n)
+void _display(unsigned short n)
 {
   if (n < 10) {
     printf("%1X", table[n]);
     return;
   }
-  display(n / 10);
+  _display(n / 10);
   printf("%1X", table[n % 10]);
+}
+
+void display(unsigned short n)
+{
+  _display(n);
+  printf("\n");
 }
 
 int main(int argc, const char *argv[]) 
 {
   display(100);
+  display(1);
+  display(0);
+  display(0xFFFF);
   return 0;
 }
