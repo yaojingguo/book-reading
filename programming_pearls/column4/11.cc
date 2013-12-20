cbe16d8d94ccfa7dbd4c7b6672952403a555fb51
Jingguo Yao <yaojingguo@gmail.com>
2013-12-20 14:41:01 +0800

    Up

--- programming_pearls/column4/11.cc
+++ programming_pearls/column4/11.cc
@@ -2,17 +2,18 @@
 #include <assert.h>
 
 #define MAXN 100
-typedef int DataType;
+typedef unsigned DataType;
 
 int n;
 DataType x[MAXN];
 
+// Return value instead of index. -1 means not-found.
 int binarysearch(DataType x[], int n, DataType t)
 {
   if (n < 1) return -1;
   int m = (n - 1) / 2;
   if (t == x[m]) 
-    return m;
+    return x[m];
   else if (t > x[m])  
     return binarysearch(x + m + 1, n - (m + 1), t);
   else             
@@ -24,6 +25,6 @@ int main(int argc, const char *argv[])
   n = 10;
   for (int i = 0; i < n; i++)
     x[i] = 10 * i;
-  assert(binarysearch(x, 10, 30) == 3);
+  printf("%d\n", binarysearch(x, 10, 30));
   return 0;
 }
