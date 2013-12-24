48242184768f9e0199917a868a59c12cc62817ba
Jingguo Yao <yaojingguo@gmail.com>
2013-12-24 10:30:18 +0800

    Up

--- programming_pearls/column8/12.cc
+++ programming_pearls/column8/12.cc
@@ -10,14 +10,13 @@ void slice_add(int x[], struct addition opts[], int n)
 {
   int *p = new int[n + 1];
   int *cum = p + 1;
+
   for (int i = 0; i < n; i++) {
     cum[opts[i].l - 1] -= opts[i].v;
     cum[opts[i].u] += opts[i].v;
   }
-
-  for (int i = n - 1; i >= 0; i--) {
+  for (int i = n - 1; i >= 0; i--) 
     x[i] = x[i + 1] + cum[i];
-  }
     
   delete [] p;
 }
@@ -35,6 +34,7 @@ void test1()
   struct addition opts[] = {{0, 2, 7}, {2, 4, 8}, 
                             {1, 3, 1}, {2, 3, 7}, 
                             {1, 2, -1}, {3, 5, 2}};
+  // The last item is a holder
   int n = 6;
   slice_add(x, opts, n);  
   info(x, n);
@@ -44,6 +44,7 @@ void test2()
 {
   int x[3] = {0};
   struct addition opts[] = {{0, 1, 2}, {0, 0, 10}};
+  // The last item is a holder
   int n = 2;
   slice_add(x, opts, n);
   info(x, n);
@@ -51,7 +52,6 @@ void test2()
 
 int main(int argc, const char *argv[]) 
 {
-  // The last item is a holder
   test1();
   test2();
   return 0;
