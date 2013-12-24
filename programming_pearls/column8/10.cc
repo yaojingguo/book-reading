bcee6911948bfca065990ac36cb23e61fad95cb7
Jingguo Yao <yaojingguo@gmail.com>
2013-12-24 20:37:17 +0800

    Up

--- programming_pearls/column8/10.cc
+++ programming_pearls/column8/10.cc
@@ -1,47 +1,52 @@
 #include <map>
-#include <iostream>
+#include <stdio.h>
 #include <algorithm>
 #include <limits.h>
 
 using namespace std;
 
-// Return i and j with A[i..j]
-pair<int, int> nearest_to_c(int t, int n, int A[]) {
-  map<int, int> bst;
-  bst[0] = -1;
-  // barriers assume that no prefix sum is equal to INT_MAX or INT_MIN
-  bst[INT_MIN] = -1;
-  bst[INT_MAX] = n;
+#define IDX_LOW_BOUND -2
 
-  int presum, subsum, i, j, start, end;
-  int closest;
+// Return i and j with A[i..j]
+pair<int, int> nearest_to_c(int A[], int n, int t) {
+  map<int, int> bst;
+  int presum, subsum, closest, i, j, start, end;
   bool unset;
   map<int, int>::iterator it;
+
+  bst[0] = -1;
+  // Barriers. Assume that no prefix sum is equal to INT_MAX or INT_MIN.
+  bst[INT_MIN] = IDX_LOW_BOUND;
+  bst[INT_MAX] = n;
   unset = true;
+  closest = INT_MAX;
+  presum = 0;
   for (i = 0; i < n; ++i) {
     presum += A[i];
     for (it = bst.lower_bound(presum - t), j = 0; j < 2; --it, j++) {
-      if (it->first == -1 or it->first == n) 
+      if (it->first == INT_MAX || it->first == INT_MIN) 
         continue;
       subsum = presum - it->first;
       if (unset || abs(closest - t) > abs(subsum - t)) {
         closest = subsum;
         start = it->second + 1;
         end = i;
+        if (closest - t == 0)
+          goto ret;
         unset = false;
       }
     }
     bst[presum] = i;
   }
+ret:
   return make_pair(start, end);
 }
 
-// demo
 int main() {
   int A[] = {10, 20, 30, 30, 20, 10, 10, 20};
   int t;
-  cin >> t;
-  pair<int, int> ans = nearest_to_c(t, 8, A);
-  cout << ans.first << ' ' << ans.second << endl;
+  scanf("%d", &t);
+  pair<int, int> ans = nearest_to_c(A, 8, t);
+  printf("[%d:%d]\n", ans.first, ans.second);
   return 0;
 }
