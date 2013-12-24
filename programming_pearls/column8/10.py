341a168963711219ae894f7977037cec49856cf6
Jingguo Yao <yaojingguo@gmail.com>
2013-12-24 16:20:23 +0800

    Up

--- programming_pearls/column8/10.py
+++ programming_pearls/column8/10.py
@@ -5,16 +5,17 @@ def nearest_to_zero(A):
   p = [[0, i] for i in xrange(n + 1)]
   for i in xrange(1, n + 1):
     p[i][0] = p[i - 1][0] + A[i - 1]
-  p.sort(key = lambda x : x[0])
+  p.sort(key = lambda x: x[0])
   nearest = sys.maxint
   pos = (None, None)
   for i in xrange(n):
     diff = p[i + 1][0] - p[i][0]
     if nearest > diff:
       nearest = diff
-      pos = (p[i + 1][1], p[i][1])
+      pos = (p[i+1][1], p[i][1])
   pos = (min(pos), max(pos))
   return (nearest, pos)
+
 
 A = [1, 2, -5, 6, 7, -3, -4]
 print(nearest_to_zero(A))
