88df3da0e2dcc4b756e03c71ea4c5a6cd53c6bef
Jingguo Yao <yaojingguo@gmail.com>
2013-12-25 10:26:27 +0800

    Up

--- programming_pearls/column9/6.cc
+++ programming_pearls/column9/6.cc
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <assert.h>
+
+#define BETWEEN(c, l, u) ((l) <= (c) && (c) <= (u))
+
+int isupper(int c)
+{
+  return BETWEEN(c, 'A', 'Z');
+}
+
+int isdigit(int c)
+{
+  return BETWEEN(c, '0', '9');
+}
+
+int islower(int c)
+{
+  return BETWEEN(c, 'a', 'z');
+}
 
 int main(int argc, const char *argv[]) 
 {
-  
+  assert(isdigit('9') == 1);
+  assert(isdigit('-') == 0);
+  assert(isupper('A') == 1);
+  assert(isupper('@') == 0);
+  assert(islower('s') == 1);
+  assert(islower('*') == 0);
   return 0;
 }
