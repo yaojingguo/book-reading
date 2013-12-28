ae88ac702abeb14ae1bcee575c7a7eac37a650fc
Jingguo Yao <yaojingguo@gmail.com>
2013-12-28 14:33:49 +0800

    Up

--- programming_pearls/column10/4.cc
+++ programming_pearls/column10/4.cc
@@ -1,10 +1,52 @@
 #include <stdio.h>
 #include <stack>
+#include <algorithm>
 
 using namespace std;
 
-void qsort(int a[], int n)
+struct range {
+  int l;
+  int u;
+};
+
+void qsort(int a[], int l, int u)
 {
+  stack<range> st;
+  int m, i;
+  range r = {l, m};
+  st.push(r);
+  while (!st.empty()) {
+    l = st.top().l;
+    u = st.top().u;
+    st.pop();
+    if (l >= u)
+      continue;
+    m = l;
+    for (i = l + 1; i <= u; i++)
+      if (a[i] < a[l])
+        swap(a[++m], a[i]);
+    swap(a[l], a[m]);
+    if (m - 1 - l <= u - m - 1) {
+      r.l = l; 
+      r.u = m - 1;
+      st.push(r);
+      r.l = m + 1; 
+      r.u = u;
+      st.push(r);
+    } else {
+      r.l = m + 1; 
+      r.u = u;
+      st.push(r);
+      r.l = l; 
+      r.u = m - 1;
+      st.push(r);
+    }
+  }
+}
+
+void sort(int a[], int n)
+{
+  qsort(a, 0, n - 1);
 }
 
 void info(int a[], int n)
@@ -18,6 +60,7 @@ int main(int argc, const char *argv[])
 {
   int a[] = {8, 1, 2, 6, 5, 3};
   int n = 6;
-  qsort(a, n);
+  sort(a, n);
+  info(a, n);
   return 0;
 }
