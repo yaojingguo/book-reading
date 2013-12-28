39e0a4bcf384f4c6ba72f21f67cf3bb0b7fc1710
Jingguo Yao <yaojingguo@gmail.com>
2013-12-28 14:13:35 +0800

    Up

--- programming_pearls/column10/4.cc
+++ programming_pearls/column10/4.cc
@@ -1,5 +1,23 @@
+#include <stdio.h>
+#include <stack>
+
+using namespace std;
+
+void qsort(int a[], int n)
+{
+}
+
+void info(int a[], int n)
+{
+  for (int i = 0; i < n; i++)
+    printf("%d ", a[i]);
+  printf("\n");
+}
+
 int main(int argc, const char *argv[]) 
 {
-  
+  int a[] = {8, 1, 2, 6, 5, 3};
+  int n = 6;
+  qsort(a, n);
   return 0;
 }
