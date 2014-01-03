fcc78fd59ff80ab095137721a441122e5509bc34
Jingguo Yao <yaojingguo@gmail.com>
2014-01-03 12:54:47 +0800

    Up

--- programming_pearls/column14/7.cc
+++ programming_pearls/column14/7.cc
@@ -14,11 +14,9 @@ int log(unsigned n)
 
 void build(int l, int u, int table[], int i)
 {
-  if (l > u) {
-    table[i] = -1;
-    return;
-  }
+  if (l > u) return;
   int m = (u - l) / 2 + l;
+  printf("l: %d, u: %d, i: %d, m: %d\n", l, u, i, m);
   table[i] = m;
   build(l, m - 1, table, 2*i);
   build(m + 1, u, table, 2*i+1);
@@ -29,6 +27,8 @@ int* build_table(int n)
   int l = log(n);
   int size = 1 << (l+1);
   int* table = new int[size];
+  for (int i = 1; i < size; i++)
+    table[i] = -1;
   build(0, n - 1, table, 1);
   return table;
 }
@@ -45,11 +45,10 @@ int main(int argc, const char *argv[])
   int* table;
   int n;
 
-  n = 7;
-  table = build_table(n);
-  info(table, n);
-  delete [] table;
-  // table = build_table(6);
-  // in
+  for (n = 6; n <= 6; n++) {
+    table = build_table(n);
+    info(table, 7);
+    delete [] table;
+  }
   return 0;
 }
