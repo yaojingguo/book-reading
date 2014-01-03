7cb013d09901e77c89f0b9e61af575aac6bddc3e
Jingguo Yao <yaojingguo@gmail.com>
2014-01-03 14:14:06 +0800

    Up

--- programming_pearls/column15/wordfreq.c
+++ programming_pearls/column15/wordfreq.c
@@ -9,9 +9,9 @@
 
 typedef struct node *nodeptr;
 typedef struct node {
-	char *word;
-	int count;
-	nodeptr next;
+  char *word;
+  int count;
+  nodeptr next;
 } node;
 
 #define NHASH 29989
@@ -19,11 +19,11 @@ typedef struct node {
 nodeptr bin[NHASH];
 
 unsigned int hash(char *p)
-{	
+{ 
   unsigned int h = 0;
-	for ( ; *p; p++)
-		h = MULT * h + *p;
-	return h % NHASH;
+  for ( ; *p; p++)
+    h = MULT * h + *p;
+  return h % NHASH;
 }
 
 #define NODEGROUP 1000
@@ -31,13 +31,13 @@ int nodesleft = 0;
 nodeptr freenode;
 
 nodeptr nmalloc()
-{	
+{ 
   if (nodesleft == 0) {
-		freenode = malloc(NODEGROUP*sizeof(node));
-		nodesleft = NODEGROUP;
-	}
-	nodesleft--;
-	return freenode++;
+    freenode = (nodeptr) malloc(NODEGROUP*sizeof(node));
+    nodesleft = NODEGROUP;
+  }
+  nodesleft--;
+  return freenode++;
 }
 
 #define CHARGROUP 10000
@@ -45,44 +45,44 @@ int charsleft = 0;
 char *freechar;
 
 char *smalloc(int n)
-{	
+{ 
   if (charsleft < n) {
-		freechar = malloc(n+CHARGROUP);
-		charsleft = n+CHARGROUP;
-	}
-	charsleft -= n;
-	freechar += n;
-	return freechar - n;
+    freechar = malloc(n+CHARGROUP);
+    charsleft = n+CHARGROUP;
+  }
+  charsleft -= n;
+  freechar += n;
+  return freechar - n;
 }
 
 void incword(char *s)
-{	
+{ 
   nodeptr p;
-	int h = hash(s);
-	for (p = bin[h]; p != NULL; p = p->next)
-		if (strcmp(s, p->word) == 0) {
-			(p->count)++;
-			return;
-		}
-	p = nmalloc();
-	p->count = 1;
-	p->word = smalloc(strlen(s)+1);
-	strcpy(p->word, s);
-	p->next = bin[h];
-	bin[h] = p;
+  int h = hash(s);
+  for (p = bin[h]; p != NULL; p = p->next)
+    if (strcmp(s, p->word) == 0) {
+      (p->count)++;
+      return;
+    }
+  p = nmalloc();
+  p->count = 1;
+  p->word = smalloc(strlen(s)+1);
+  strcpy(p->word, s);
+  p->next = bin[h];
+  bin[h] = p;
 }
 
 int main()
-{	
+{ 
   int i;
-	nodeptr p;
-	char buf[100];
-	for (i = 0; i < NHASH; i++)
-		bin[i] = NULL;
-	while (scanf("%s", buf) != EOF)
-		incword(buf);
-	for (i = 0; i < NHASH; i++)
-		for (p = bin[i]; p != NULL; p = p->next)
-			printf("%s %d\n", p->word, p->count);
-	return 0;
+  nodeptr p;
+  char buf[100];
+  for (i = 0; i < NHASH; i++)
+    bin[i] = NULL;
+  while (scanf("%s", buf) != EOF)
+    incword(buf);
+  for (i = 0; i < NHASH; i++)
+    for (p = bin[i]; p != NULL; p = p->next)
+      printf("%s %d\n", p->word, p->count);
+  return 0;
 }
