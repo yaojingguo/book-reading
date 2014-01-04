326e42a6f77c49c1100288527b0f2b4158ea2d4d
Jingguo Yao <yaojingguo@gmail.com>
2014-01-04 14:59:22 +0800

    Up

---
+++ programming_pearls/column15/5.cc
@@ -0,0 +1,46 @@
+
+/* Copyright (C) 1999 Lucent Technologies */
+/* From 'Programming Pearls' by Jon Bentley */
+
+/* wordfreq.cpp -- List all words in input file, with counts */
+
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+struct node {
+  string s;
+  struct node* next;
+};
+
+int main()
+{ 
+  map<string, int> M;
+  map<string, int>::iterator j;
+  string s;
+  while (cin >> s)
+    M[s]++;
+
+  map<int, node*> T;
+  node** p;
+  int cnt;
+  for (j = M.begin(); j != M.end(); ++j) {
+    s = j->first;
+    cnt = j->second;
+    for (p = &T[cnt]; (*p) != NULL; p = &((*p)->next))
+      ;
+    (*p) = new node();
+    (*p)->s = s;
+    (*p)->next = NULL;
+  }
+
+  map<int, node*>::reverse_iterator k;
+  node* q;
+  for (k = T.rbegin(); k != T.rend(); ++k) {
+    cnt = k->first;
+    for (q = k->second; q != NULL; q = q->next)
+      cout << q->s << " " << cnt << endl;
+  }
+  return 0;
+}
